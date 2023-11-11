58ab941ff6517024394598ca9b6dcfc446b3db96
LGgameLAB <45958481+LGgameLAB@users.noreply.github.com>
2023-11-10 16:27:55 -0800

    Update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.cmd[[set autoindent expandtab tabstop=4 shiftwidth=4]]
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
